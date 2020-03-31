bf9591b825a492d442c186b7463ef8a50ef18a93
Darren <953774303@qq.com>
2020-03-31 09:12:31 +0800

    mybatis作业提交

---
+++ src/main/java/com/lagou/dao/IUserDao.java
@@ -0,0 +1,23 @@
+package com.lagou.dao;
+
+import com.lagou.pojo.User;
+
+import java.util.List;
+
+public interface IUserDao {
+
+    //查询所有用户
+    public List<User> findAll() throws Exception;
+
+
+    //根据条件进行用户查询
+    public User findByCondition(User user) throws Exception;
+
+    //增加用户
+    public void add(User user);
+
+    public void update(User user);
+
+    public void delete(User user);
+
+}
